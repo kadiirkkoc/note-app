b04dce68adc0bbae231dd7e8ac463e41ca839787
Kadir Koç <kadiirkkoc@gmail.com>
2023-05-28 12:18:51 +0200

    sidebar and app.js is done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React , {useState} from "react";
+import {nanoid} from "nanoid"
+import Sidebar from "./components/Sidebar";
+import Split from "react-split"
+import Editor from "./components/Editor";
+
+export default function App() {
+
+  const [notes,setNotes] = useState([])
+  const [currentNoteId,setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ""
+  )
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "Type your note's title here"
+    }
+
+    setNotes(prevNotes => [newNote,...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
+  function updateNote(text){
+    setNotes(oldNotes => oldNotes.map(oldNote => {
+        return oldNote.id === currentNoteId ? {...oldNote, body: text} : oldNote
+    }))
+  }
+
+  function findCurrentNode(){
+    return notes.find(note => {
+      return note.id === currentNoteId
+    }) || notes[0]
+  }
+
+
   return (
-    <div className="App">
-      start
-    </div>
+    <main>
+      {
+        notes.length > 0 ?
+        <Split
+            sizes={[30,70]}
+            direction="horizontal"
+            className="split"
+        >
+            <Sidebar
+                notes={notes}
+                currentNote={findCurrentNode()}
+                setCurrentNoteId={setCurrentNoteId}
+                newNote={createNewNote}
+            />
+            {
+              currentNoteId && 
+              notes.length > 0 && 
+              <Editor 
+                  currentNote={findCurrentNode()}
+                  updateNote={updateNote}
+              />
+            }
+        </Split>
+        :
+
+            <div className="no-notes">
+                <h1>You have no notes</h1>
+                <button 
+                    className="first-note" 
+                    onClick={createNewNote}
+                >
+                    Create one now
+                </button>
+            </div>
+      }
+    </main>
   );
 }
 
-export default App;
---
+++ src/components/Editor.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import ReactMde from "react-mde"
+
+export default function Editor(){
+
+    const [selectedTab,setSelectedTab] = useState("write")
+
+    return(
+        <div>kadir</div>
+    )
+}
