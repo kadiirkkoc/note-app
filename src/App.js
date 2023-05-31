98edcfacea4171051abe741a540482260031fb6a
Kadir Koç <kadiirkkoc@gmail.com>
2023-05-31 21:33:22 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,66 +1,91 @@
-
-import React , {useState} from "react";
-import {nanoid} from "nanoid"
-import Sidebar from "./components/Sidebar";
+import React, { useState , useEffect } from "react"
+import Sidebar from "./components/Sidebar"
+import Editor from "./components/Editor"
+import { data } from "./data"
 import Split from "react-split"
-import Editor from "./components/Editor";
+import {nanoid} from "nanoid"
 
 export default function App() {
+   
+    
+  const [notes, setNotes] = React.useState(() => {
+    const storedNotes = JSON.parse(localStorage.getItem("notes"));
+    return Array.isArray(storedNotes) ? storedNotes : [];
+    });
+    
+    const [currentNoteId, setCurrentNoteId] = useState(
+        (notes[0] && notes[0].id) || ""
+    )
+    
+    useEffect(() => {
+      localStorage.setItem("notes",JSON.stringify(notes))
+    },[notes])
 
-  const [notes,setNotes] = useState([])
-  const [currentNoteId,setCurrentNoteId] = useState(
-    (notes[0] && notes[0].id) || ""
-  )
-
-  function createNewNote() {
-    const newNote = {
-      id: nanoid(),
-      body: "Type your note's title here"
+    function createNewNote() {
+        const newNote = {
+            id: nanoid(),
+            body: "# Type your markdown note's title here"
+        }
+        setNotes(prevNotes => [newNote, ...prevNotes])
+        setCurrentNoteId(newNote.id)
+        
+    }
+    
+    function updateNote(text) {
+        setNotes(oldNotes => {
+          const newArray = []
+          for(let i=0;i<oldNotes.length;i++){
+            const oldNote = oldNotes[i]
+            if(oldNote.id === currentNoteId){
+               newArray.unshift({...oldNote , body : text})
+            }
+            else {
+               newArray.push(oldNote)
+            }
+          }
+          return newArray
+        })
     }
 
-    setNotes(prevNotes => [newNote,...prevNotes])
-    setCurrentNoteId(newNote.id)
-  }
-
-  function updateNote(text){
-    setNotes(oldNotes => oldNotes.map(oldNote => {
-        return oldNote.id === currentNoteId ? {...oldNote, body: text} : oldNote
-    }))
-  }
-
-  function findCurrentNode(){
-    return notes.find(note => {
-      return note.id === currentNoteId
-    }) || notes[0]
-  }
-
-
-  return (
-    <main>
-      {
-        notes.length > 0 ?
-        <Split
-            sizes={[30,70]}
-            direction="horizontal"
-            className="split"
-        >
-            <Sidebar
-                notes={notes}
-                currentNote={findCurrentNode()}
-                setCurrentNoteId={setCurrentNoteId}
-                newNote={createNewNote}
-            />
-            {
-              currentNoteId && 
-              notes.length > 0 && 
-              <Editor 
-                  currentNote={findCurrentNode()}
-                  updateNote={updateNote}
-              />
-            }
-        </Split>
-        :
-
+    function deleteNote(event,noteId){
+       event.stopPropagation()
+       setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
+       console.log(notes)
+    }
+    
+    function findCurrentNote() {
+        return notes.find(note => {
+            return note.id === currentNoteId
+        }) || notes[0]
+    }
+    
+    return (
+        <main>
+        {
+            notes.length > 0 
+            ?
+            <Split 
+                sizes={[30, 70]} 
+                direction="horizontal" 
+                className="split"
+            >
+                <Sidebar
+                    notes={notes}
+                    currentNote={findCurrentNote()}
+                    setCurrentNoteId={setCurrentNoteId}
+                    newNote={createNewNote}
+                    deleteNote={deleteNote}
+                />
+                {
+                    currentNoteId && 
+                    notes.length > 0 &&
+                    <Editor 
+                        currentNote={findCurrentNote()} 
+                        updateNote={updateNote} 
+                    />
+                }
+            </Split>
+            :
             <div className="no-notes">
                 <h1>You have no notes</h1>
                 <button 
@@ -70,8 +95,8 @@ export default function App() {
                     Create one now
                 </button>
             </div>
-      }
-    </main>
-  );
+            
+        }
+        </main>
+    )
 }
-
